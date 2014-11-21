a7436d52a3c74c2728dc00189aac19f5fbbbaef1
misterashley <github@mrashley.com>
2014-11-20 17:29:07 -0800

    End of the day
    Just to be sure. I think it's all fine and stuff now.

--- image resizer.py
+++ image resizer.py
@@ -116,15 +116,15 @@ def process_images():
             if canvas: #apply a white background with the image centered
 ##                print('canvas')
                 if max(dimensions) < max(hero_dimensions):
-##                    print("teeny picture:"+filename)
+                    print("teeny picture:"+filename)
                     canvas_image('"'+filename+'"',hero_dimensions[0],hero_dimensions[1],'"'+newfilename+'"')
                 elif max(dimensions) > max(max_dimensions):
-##                    print("biggy picture:"+filename)
+                    print("biggy picture:"+filename)
                     canvas_image('"'+filename+'"',max_dimensions[0],max_dimensions[1],'"'+newfilename+'"')
-                elif dimensions[0]/dimensions[1]== max_dimensions[0]/max_dimensions[1]:
+                elif dimensions[0]/dimensions[1]== max_dimensions[0]/max_dimensions[1]:pass
 ##                    print('nothing to do here')
                 else:
-##                    print("normal picture:"+filename)
+                    print("normal picture:"+filename)
                     canvas_image('"'+filename+'"',max(dimensions),max(dimensions),'"'+newfilename+'"')
 
             if image_format: # convert to another format
