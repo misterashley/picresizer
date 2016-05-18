ec3ec9ceb91ca963f96cda7bc03518aed2efbb33
misterashley <github@mrashley.com>
2016-05-18 14:30:51 -0700

    Stripping and quality %
    This version introduces stripping the EXIF and pushing a quality in
    percent to the item, both options.
    I tidied up some syntax, and made it so we can turn on and off verbose
    information including being able to see the command line entirely.

--- image resizer.py
+++ image resizer.py
@@ -4,14 +4,14 @@
 ## or grow images that are smaller than the hero image default.
 
 #user config
-bksource =r"C:\Users\mrashley\Dropbox\XM Docs\Ennco\images" #the r is for 'raw' allowing \ 
+bksource =r"D:\BitTorrent Sync\ALL SEASONS IMAGES\new carhartt images" #the r is for 'raw' allowing \ 
 source = bksource.replace('\\','/')
 if source[-1] != '/':
 	source = source + '/'
 #source = "C:/Users/mrashley/Dropbox/Photos for website/" # must have trailing /
 
 max_dimensions = (1000,1000)
-hero_dimensions = (393,484)
+hero_dimensions = (400,400)
 #force_hero_size
 #True means we will upsize the image's largest edge a hero dimension
 #False means we will leave the image at it's native size
@@ -20,9 +20,17 @@ force_to_hero_size = True
 #True means we will put the image on a canvas for a consistent image ratio
 #False means we will only resize large images
 canvas = True
+
+quality_percent = 85  #jpg percent quality // set quality_percent to 0 (zero) to disable.... %92 is pretty good, %85 is okay.
+
+strip = True #strip exif tag // True to enable, False to disable
+
 image_format = ".jpg" #False for no conversion, or ".jpg", ".png", ".gif"
 #string of ".jpg" or ".png" or ".gif" to convert files to this format.
+
 resize = True #False for no resize
+
+reporting = False #False for minor reporting. True for lots of reporting
 
 #----------------------- no config below this -----------------
 
@@ -30,7 +38,7 @@ import os, sys, subprocess
 #import shutil
 from PIL import Image
 global counter
-counter = dict(touched=0, grown=0, shrunk=0, canvased=0, converted=0)
+counter = dict(touched=0, grown=0, shrunk=0, canvased=0, converted=0, stripped=0, adj_qual=0)
 
 si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW # this is to hide the command line
@@ -43,16 +51,17 @@ def get_image_dimensions(filename):
     except IOError: print(filename,"is not an image.")
 
 def shrink_to_bounds(filename,width,height,newfilename):
-    print("convert " + filename + ' -resize '+str(width)+'x'+str(height)+' '+newfilename)
-    subprocess.call("convert " + filename + ' -resize '+str(width)+'x'+str(height)+' '+newfilename, startupinfo=si, shell=True)
+    argument = str("convert " + filename + ' -resize '+str(width)+'x'+str(height)+' '+ newfilename)
+    if reporting: print("Shrinking: " + argument)
+    subprocess.call(argument, startupinfo=si, shell=True)
     counter['shrunk'] += 1
 
 
 def grow_to_bounds(filename,width,height,newfilename):
-    print("convert " + filename + ' -resize '+str(width)+'x'+str(height)+' '+newfilename)
-    subprocess.call("convert " + filename + ' -resize '+str(width)+'x'+str(height)+\
-                       ' '+newfilename, startupinfo=si, shell=True)
-    counter['grown'] += 1 
+    argument = str("convert " + filename + ' -resize '+str(width)+'x'+str(height)+' '+newfilename) 
+    if reporting: print("Growing: " + argument)
+    subprocess.call(argument, startupinfo=si, shell=True)
+    counter['grown'] += 1
 
 def squareoff_image():
     pass
@@ -60,37 +69,49 @@ def squareoff_image():
 def canvas_image(filename,width,height,newfilename):
 ##            this is the imagemagick command to put on a white border
 ##            convert input.jpg -gravity center -background white -extent 250x250  output.jpg
-    subprocess.call("convert "+ filename + " -gravity center -background white -extent "+ \
-          str(width)+"x"+str(height)+" "+newfilename, startupinfo=si, shell=True)
-    counter['canvased'] += 1
+    argument = str("convert "+ filename + " -gravity center -background white -extent "+ \
+          str(width)+"x"+str(height)+" "+newfilename)
+    if reporting: print(argument)
+    if subprocess.call(argument, startupinfo=si, shell=True): counter['canvased'] += 1
 
 def format_convert(filename,image_format):
-##        print(filename,image_format)
-        try:
-            file_and_ext = os.path.splitext(filename)
-            if file_and_ext[1].lower() == image_format:
-##                print("same extension")
-                pass
-            else:
-##                print('convert "' + filename + '" -background white -flatten "' + file_and_ext[0] + image_format + '"')
-
-                if subprocess.call('convert "' + filename + '" -background white -flatten ' + \
-                                file_and_ext[0] + image_format + '"', startupinfo=si, shell=True) : counter['converted'] += 1
-                #do the conversion, and if exits with process 0, then register this as converted.
-                pass #delete the old filename also, please
-                    
-        except:
-            print("Format conversion failed")
+    try:
+        file_and_ext = os.path.splitext(filename)
+        if reporting: print(str(file_and_ext[0],image_format))
+        if file_and_ext[1].lower() == image_format:
+            if reporting: print(str("same extension: " + image_format))
+            pass
+        else:
+            argument = str('convert "' + filename + '" -background white -flatten "' + file_and_ext[0] + image_format + '"')
+            if reporting: print(argument)
+            if subprocess.call(argument, startupinfo=si, shell=True): counter['converted'] += 1
+            #do the conversion, and if exits with process 0, then register this as converted.
+            else: print("This file could not be converted. Perhaps it is not a graphic.") #delete the old filename also, please
+                
+    except:
+        print("No format conversion.")
 ##
 
 def reduce_filesize(filename,filesize):
         #to define max file size "convert file.jpg -define jpeg:extent:500KB newfile.jpg"
     pass
+def strip_exif(filename,newfilename):
+    argument = str('convert "'+ filename + '" -strip "' + newfilename + '"')
+    if reporting: print(argument)
+    subprocess.call(argument, startupinfo=si, shell=True)
+    counter['stripped'] += 1
+
+def set_image_quality(filename,quality_percent,newfilename):
+    argument = str('convert "' + filename + '" -quality '+str(quality_percent)+' "'+newfilename+'"')
+    if reporting: print(argument)
+    subprocess.call(argument, startupinfo=si, shell=True)
+    counter['adj_qual'] += 1
 
 def process_images():
     file_count = sum(1 for f in os.listdir())
     for filename in os.listdir(source):
-##        print (type(file_count),type(counter[0]))
+        if reporting: print("-------------------------------")
+        if reporting:   print (type(file_count),type(counter))
         sys.stdout.flush()
         if counter['touched'] % 50 == 0:
             sys.stdout.write('\r'+str(file_count - counter['touched']) +" files left to go. Working on " + filename + 15 * " "+"\n")
@@ -103,7 +124,7 @@ def process_images():
             dimensions = get_image_dimensions(source + filename)
             #Shrink huge image to max dimensions
             if resize:
-##                print('resize')
+                if reporting: print('resize')
                 if max(dimensions) > max(max_dimensions): #picture is bigger than max_dimensions
                     shrink_to_bounds('"'+filename+'"',max_dimensions[0],max_dimensions[1],'"'+newfilename+'"')
 
@@ -115,7 +136,7 @@ def process_images():
                 else: pass
         
             if canvas: #apply a white background with the image centered
-##                print('canvas')
+                if reporting: print('canvas')
                 if max(dimensions) < max(hero_dimensions):
                     print("teeny picture:"+filename)
                     canvas_image('"'+filename+'"',hero_dimensions[0],hero_dimensions[1],'"'+newfilename+'"')
@@ -129,8 +150,16 @@ def process_images():
                     canvas_image('"'+filename+'"',max(dimensions),max(dimensions),'"'+newfilename+'"')
 
             if image_format: # convert to another format
-##                print('format')
+                if reporting: print('format')
                 format_convert(filename,image_format)
+            if strip:
+                    if reporting: print("stripping exif")
+                    strip_exif(filename,newfilename)
+            if quality_percent:
+                    if reporting: print("quality set to %",quality_percent)
+                    set_image_quality(filename,quality_percent,newfilename)
+                
+                
 
         
         except Exception as e: print(str(e))
@@ -147,6 +176,8 @@ if __name__ == "__main__":
     os.chdir(source)
     print("Working in",str(os.getcwd()))
     process_images()
+    print("Finished. Going up a level.")
+    os.chdir("..") #change director to up one level, so the directory is unlocked
 
 ##    canvas the new images -
 
