585c08c48b18994318760ed98d203cff66414727
misterashley <github@mrashley.com>
2018-09-09 15:52:29 -0700

    Starting scanNplan pseudo coding

---
+++ scanNplan.py
@@ -0,0 +1,31 @@
+## scanNplan is part of version 2 of picresizer
+## https://github.com/misterashley/picresizer
+##
+## Get a file selection (currently from config file, later from GUI)
+##
+## Get set of option to use, set these globally
+##
+## Pass the above to scanNplan, and build a VFO
+##    (a list of verified files, with the options to work on)
+##
+## File selector
+##   - Choose a folder
+##       - Option to delve into subfolders
+##   - Choose a file
+## Options (set globally)
+##   - Maximum desired output image dimension (Desired ratio is a function of max output)
+##   - Minimum desired output image dimensions
+##   - Resize images
+##   - Canvas enabled
+##   - Canvas colour ? (Hex value, colour wheel perhaps)
+##   - Compression enabled / percent
+##   - Strip EXIF data to shrink filesize
+##   - Convert to .JPG
+##       - Delete original image upon successful conversion
+##   - Preserve .PNG images in that format
+##   - Debug to console
+
+
+#import config file
+
+    
