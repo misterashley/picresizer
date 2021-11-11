14d5ef2837a27a882e2ae5f63516daf01f73c635
misterashley <misterashley@users.noreply.github.com>
2021-11-10 16:28:06 -0800

    Updating
    I wish I could understand what Github doesn't like about this merge. Forcing in my rejected updates with the web interface.

--- GUIgoodness.py
+++ GUIgoodness.py
@@ -2,69 +2,90 @@ import os
 from tkinter import filedialog # For browsing to the directory with tkinter directory chooser
 #from tkinter import *
 import tkinter as tk
-from time import sleep # for update_idletasks
-import scan_n_plan #werkwerkwerk
+#from time import sleep # for update_idletasks
+import scan_n_plan, werkwerkwerk
 
 def analyse_folder_of_files():
 	pass
 
+
 def close_window():
-    window.destroy()
+    root.destroy()
     exit()
 
-def browse_button():
+def select_folder():
+        try: buttonProcessImages.destroy()
+        except: pass
         selected_directory.set(filedialog.askdirectory())
-##trying to get the label to update.
-        window.update_idletasks()
-        #global files_to_check
-        files_to_check = scan_n_plan.find_files(selected_directory.get())
-        image_files = []
-        #print(type(files_to_check))
-        #print(len(files_to_check))
-        while len(files_to_check) > 0:
-                statusMessage.set("Looking at "+ str(len(files_to_check))+" files...")
-                #print(files_to_check)
-                #image_files = scan_n_plan.pop_and_check(files_to_check)
-                #scan_n_plan.check_image_dims(list_of_images)
-                files_to_check, image_files = scan_n_plan.create_list_of_images_from_file_list(files_to_check, image_files)
-                statusMessage.set(str(len(image_files)) + " images found. " + str(len(files_to_check)) + " files remaining.")
-                window.update_idletasks()
-        statusMessage.set(str(len(image_files)) + " image(s) found.")
+        if selected_directory.get() != '':
+                buttonProcessImages = tk.Button (main, text = "Process Images", width=20, command=scan)
+                buttonProcessImages.pack(pady=15)
+        print(selected_directory.get())
+        root.update_idletasks()
+        
+
+def updateInstructions(message):
+        statusMessage.set(message)
+
+def updateMessage(message):
+        stat
 
 def scan():
-        if selected_directory.get() == '':
-                statusMessage.set("Please select a folder first.")
-                return
+        try:
+                statusMessage.set("Getting list of files...")
+                #global list_of_images  ##not sure if this needs to be global. Trying to avoid that.
+                list_of_images = scan_n_plan.find_files(selected_directory.get())
+                statusMessage.set("Looking at the files...")
+                scan_n_plan.check_image_dims(list_of_images)
+                #print(selected_directory.get())
+                #statusMessage.set("...")
+                
+        except:
+                #hide the process button
+                #change the message back to select a folder
+                pass
 
-window = tk.Tk()
-window.title("picResizer")
-window.configure(background="yellow")
 
-global selected_directory
+
+#### This is the main window :
+root = tk.Tk()
+root.title("picResizer")
+root.configure(background="white")
+root.geometry('400x300')
+
+#
+##root.columnconfigure(0, weight=1) #100% of the window
+##
+##root.rowconfigure(0,weight=8) #80%
+##root.rowconfigure(1,weight=2) #20%
+
+main = tk.Frame(root, bg='white')
+footer = tk.Frame(root, bg='white')
+
+#Initialize global selected_directory
 selected_directory = tk.StringVar()
 #selected_directory.set(os.getcwd())
 #selected_directory.set("Choose a folder to scan.")
 #print(selected_directory.get())
-global statusMessage
-statusMessage = tk.StringVar()
-#statusMessage.set("...")
-#selected_folder = selected_directory.get()
 
-### get the path
-label1 = tk.Label (window, text = "Find the file folder of images to convert.", bg="white", fg="black", font="none 12")
-label1.pack(pady=10,padx=30)
-#label1.grid(row=1, column=0, sticky=W)
+#Initialize the Instructions
+labelInstructions = tk.StringVar()
+labelInstructions.set("")
+labelInstructions = tk.Label (main, textvariable = labelInstructions, bg="white", fg="black", font="none 12")
+labelInstructions.pack(pady=10)
+
+### Choose a directory. This direction is meant to have images. We'll scan subdirectories as well.
+buttonChoose = tk.Button (main, text ="Select folder", width=20, command=select_folder)
+buttonChoose.pack(pady=10)
+
+#updateInstructions("Choose a file folder of images to process")
 
 ## Selected directory
-labelPath = tk.Label (window, textvariable = selected_directory, bg ="white", fg="blue", font="none 10")
+labelPath = tk.Label (main, textvariable = selected_directory, bg ="white", fg="blue", font="none 10")
 #label2 = Label (window, text = "Current folder: \n" + selected_directory.get(), bg ="white", fg="black", font="none 12")
 labelPath.pack(pady=10, padx=5)
 #label2.grid(row=2, column=0, sticky=W)
 
-### Choose one more folders or files
-buttonChoose = tk.Button (window, text ="Choose a folder", width=20, command=browse_button)
-buttonChoose.pack(pady=10)
-#button1.grid(row=3, column=0, sticky=W)
 
 #Checkbox for Resize
 
@@ -81,12 +102,16 @@ buttonChoose.pack(pady=10)
 #Checkbox for reporting (debugging)
 
 ### run the main loop
-buttonRun = tk.Button (window, text = "Process images", width=20, command=scan)
-buttonRun.pack(pady=15)
+buttonProcessImages = tk.Button (main, text = "Process Images", width=20, command=scan)
+buttonProcessImages.pack(pady=15)
+buttonProcessImages.destroy()
+#Initialize status message
+statusMessage = tk.StringVar()
+statusMessage.set("Ready")
+labelStatusMessage = tk.Label (footer, textvariable = statusMessage, relief='sunken', fg='green', bg='black', padx='30', font="none 13")
+labelStatusMessage.pack(expand = 'True', padx='20', pady='20', anchor = 'se')
 
-labelStatusMessage = tk.Label (window, textvariable = statusMessage, bg ="white", fg="black", font="none 12")
-#label2 = Label (window, text = "Current folder: \n" + selected_directory.get(), bg ="white", fg="black", font="none 12")
-labelStatusMessage.pack(pady=10, padx=5)
+main.pack(side = 'top', expand = 'True', fill = 'both')
+footer.pack(side = 'bottom', expand = True, fill = 'both')
 
-
-window.mainloop()
+root.mainloop()
